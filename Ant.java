f6e80d707c9fbadb572ad76d9ae37d72f0fc76e2
Tomas Araujo <120048834+TomasAraujo21@users.noreply.github.com>
2024-02-17 22:12:40 -0400

    Add files via upload

---
+++ Ant.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+import EDD.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author tomasaraujo
+ */
+public class Ant {
+    private Vertex city;
+    private List pastCities;
+    private double distance;
+    private boolean arrived; // este atributo es para chequear si la hormiga llego a la ciudad destino indicada para completar el ciclo
+
+    public Ant(Vertex city, List pastCities, double distance) {
+        this.city = city;
+        this.pastCities = pastCities;
+        this.distance = distance;
+    }
+    
+    public Ant() {
+        this.city = null;
+        this.pastCities = new List();
+        this.distance = 0;
+        this.arrived = false;
+    }
+
+    /**
+     * @return the city
+     */
+    public Vertex getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(Vertex city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the pastCities
+     */
+    public List getPastCities() {
+        return pastCities;
+    }
+
+    /**
+     * @param pastCities the pastCities to set
+     */
+    public void setPastCities(List pastCities) {
+        this.pastCities = pastCities;
+    }
+
+    /**
+     * @return the distance
+     */
+    public double getDistance() {
+        return distance;
+    }
+
+    /**
+     * @param distance the distance to set
+     */
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+    
+    public boolean visited(Vertex city) {
+        // verificar que la ciudad que estoy pasando por parametro esta en la lista de ciudades recorridas de la hormiga
+        Node pValue = new Node();
+        pValue = pastCities.getpFirst();
+        for (int i = 0; i < pastCities.getSize(); i++) {
+            if (pValue.gettInfo().equals(city)) {
+                JOptionPane.showMessageDialog(null, "Esta ciudad ya ha sido visitada");
+                pValue = pValue.getpNext();
+                return true;
+            }else{
+                pValue = pValue.getpNext();
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    public void addPastCity(Vertex city) {
+        // primero verifico que esa ciudad no esta en la lista de ciudades recorridas
+        // si ya la visitó, no se hace nada. Usamos la funcion visited para eso
+        if (visited(city)) {
+            JOptionPane.showMessageDialog(null, "La ciudad ya fue visitada");
+        }else{
+            this.pastCities.addEnd(city);
+        }
+    }
+    
+    public void visitCity(Vertex city, double dist) {
+        // Primero verificamos que la ciudad que pasamos como parametro no haya sido visitada
+        // Si no ha sido visitada, la agregamos a la lista de ciudades recorridas
+        // Sumamos la distancia recorrida que actualmente += a la distancia que pasamos por parametro
+        // Y por ulitmo hacemos un setCity (set ciudad actual) para actualizarla.
+        if (visited(city)) {
+            JOptionPane.showMessageDialog(null, "La ciudad ya fue visitada");
+        }else{
+            this.pastCities.addEnd(city);
+            this.distance += dist;
+            this.setCity(city);
+        }
+    } 
+}
---
+++ SystemAnt.java
@@ -0,0 +1,221 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+import EDD.*;
+
+/**
+ *
+ * @author tomasaraujo
+ */
+public class SystemAnt {
+    // AQUI VA TODA LA INFO QUE SI ALPHA, BETA, Y ESO.
+    // LAS PRIMITIVAS VAN A SER, ACTUALIZAR LOS CICLOS, EVPARORAR FEROMONAS, ACTUALIZAR FEROMONAS.
+    
+    private List ants;
+    private double rho;
+    private double alpha;
+    private double beta;
+    private Vertex startCity;
+    private Vertex endCity;
+    private Graph graph;
+    private String optimalRoute;
+    private String pheromones;
+    private double optimalDistance;
+    private List history;       // EN EL HISTORIAL GUARDO LOS OBJETOS DE TIPO OPTIMO
+
+    public SystemAnt(List ants, double rho, double alpha, double beta, Vertex startCity, Vertex endCity, Graph graph) {
+        this.ants = ants;
+        this.alpha = alpha;
+        this.beta = beta;
+        this.startCity = startCity;
+        this.endCity = endCity;
+        this.graph = graph;
+        this.optimalRoute = "";
+        this.pheromones = "";
+        this.optimalDistance = 0; 
+        this.history = new List();
+    }
+    
+    // HACER TODOS LOS GETTER Y SETTER PARA LOS PARAMETROS
+    
+    /**
+     * @return the ants
+     */
+    public List getAnts() {
+        return ants;
+    }
+
+    /**
+     * @param ants the ants to set
+     */
+    public void setAnts(List ants) {
+        this.ants = ants;
+    }
+
+    /**
+     * @return the rho
+     */
+    public double getRho() {
+        return rho;
+    }
+
+    /**
+     * @param rho the rho to set
+     */
+    public void setRho(double rho) {
+        this.rho = rho;
+    }
+
+    /**
+     * @return the alpha
+     */
+    public double getAlpha() {
+        return alpha;
+    }
+
+    /**
+     * @param alpha the alpha to set
+     */
+    public void setAlpha(double alpha) {
+        this.alpha = alpha;
+    }
+
+    /**
+     * @return the beta
+     */
+    public double getBeta() {
+        return beta;
+    }
+
+    /**
+     * @param beta the beta to set
+     */
+    public void setBeta(double beta) {
+        this.beta = beta;
+    }
+
+    /**
+     * @return the startCity
+     */
+    public Vertex getStartCity() {
+        return startCity;
+    }
+
+    /**
+     * @param startCity the startCity to set
+     */
+    public void setStartCity(Vertex startCity) {
+        this.startCity = startCity;
+    }
+
+    /**
+     * @return the endCity
+     */
+    public Vertex getEndCity() {
+        return endCity;
+    }
+
+    /**
+     * @param endCity the endCity to set
+     */
+    public void setEndCity(Vertex endCity) {
+        this.endCity = endCity;
+    }
+
+    /**
+     * @return the graph
+     */
+    public Graph getGraph() {
+        return graph;
+    }
+
+    /**
+     * @param graph the graph to set
+     */
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+
+    /**
+     * @return the optimalRoute
+     */
+    public String getOptimalRoute() {
+        return optimalRoute;
+    }
+
+    /**
+     * @param optimalRoute the optimalRoute to set
+     */
+    public void setOptimalRoute(String optimalRoute) {
+        this.optimalRoute = optimalRoute;
+    }
+
+    /**
+     * @return the pheromones
+     */
+    public String getPheromones() {
+        return pheromones;
+    }
+
+    /**
+     * @param pheromones the pheromones to set
+     */
+    public void setPheromones(String pheromones) {
+        this.pheromones = pheromones;
+    }
+
+    /**
+     * @return the optimalDistance
+     */
+    public double getOptimalDistance() {
+        return optimalDistance;
+    }
+
+    /**
+     * @param optimalDistance the optimalDistance to set
+     */
+    public void setOptimalDistance(double optimalDistance) {
+        this.optimalDistance = optimalDistance;
+    }
+
+    /**
+     * @return the history
+     */
+    public List getHistory() {
+        return history;
+    }
+
+    /**
+     * @param history the history to set
+     */
+    public void setHistory(List history) {
+        this.history = history;
+    }
+    
+    // Primero debemos hacer una verificacion que el grafo exista, porque sin grafo no hay ningun sistema
+    public boolean GraphExists(Graph grpah) {
+        
+        return false;
+    }
+    
+    
+    // Como la distancia se inicializa en cero, para el ciclo 1 no debemos comparar con nuestro atributo distanciaOptima
+    
+    private void runSystems() {
+        
+    }
+    
+//    private void mover(Ant h) {
+//        // SE VAN A MOVER POR TODO EL GRAFO HASTA QUE LLEGUEN A LA CIUDAD FINAL 
+//        for (int i = 0; i < graph; i++) {
+//            if (h.getCity() != endCity) {
+//                // DECIDO A DONDE MOVERME (esto retorna una ciudad)
+//                // h.vistCity(ciudad retornada antes, distancia)
+//            }else{
+//                break;
+//            }
+//        }
+//    }
+}
