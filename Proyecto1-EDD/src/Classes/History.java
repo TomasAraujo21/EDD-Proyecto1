c8bb3d1fa6c2c63ce6b54db53460632232243469
Gabriel Flores <123988694+Gess04@users.noreply.github.com>
2024-02-22 01:35:30 -0400

    Add files via upload

---
+++ Proyecto1-EDD/src/Classes/History.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author Gabriel Flores
+ */
+public class History {
+    private String title;
+    private String antTrail;
+    private String optimalRoad;
+    private double optimalDistance;
+
+    public History(String title, String antTrail, String optimalRoad, double optimalDistance) {
+        this.title = title;
+        this.antTrail = antTrail;
+        this.optimalRoad = optimalRoad;
+        this.optimalDistance = optimalDistance;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAntTrail() {
+        return antTrail;
+    }
+
+    public void setAntTrail(String antTrail) {
+        this.antTrail = antTrail;
+    }
+
+    public String getOptimalRoad() {
+        return optimalRoad;
+    }
+
+    public void setOptimalRoad(String optimalRoad) {
+        this.optimalRoad = optimalRoad;
+    }
+
+    public double getOptimalDistance() {
+        return optimalDistance;
+    }
+
+    public void setOptimalDistance(double optimalDistance) {
+        this.optimalDistance = optimalDistance;
+    }
+    
+    
+}
--- Proyecto1-EDD/src/Classes/HistoryTemp.java
+++ Proyecto1-EDD/src/Classes/HistoryTemp.java
@@ -4,13 +4,50 @@
  */
 package Classes;
 import EDD.*;
+
 /**
  *
- * @author 58414
+ * @author tomasaraujo
  */
 public class HistoryTemp {
     private int numAnt;
-    private List roadAnt;
-    private String distnaceRecorrida;
+    private String roadTraveled;
+    private String lengthTraveled;
+
+    public HistoryTemp(int numAnt, String roadTraveled, String lengthTraveled) {
+        this.numAnt = numAnt;
+        this.roadTraveled = roadTraveled;
+        this.lengthTraveled = lengthTraveled;
+    }
+
+    public int getNumAnt() {
+        return numAnt;
+    }
+
+    public void setNumAnt(int numAnt) {
+        this.numAnt = numAnt;
+    }
+
+    public String getRoadTraveled() {
+        return roadTraveled;
+    }
+
+    public void setRoadTraveled(String roadTraveled) {
+        this.roadTraveled = roadTraveled;
+    }
+
+    public String getLengthTraveled() {
+        return lengthTraveled;
+    }
+
+    public void setLengthTraveled(String lengthTraveled) {
+        this.lengthTraveled = lengthTraveled;
+    }
+
+    @Override
+    public String toString() {
+        return "HistoryTemp{" + "numAnt=" + numAnt + ", roadTraveled=" + roadTraveled + ", lengthTraveled=" + lengthTraveled + '}';
+    }
+    
     
 }
